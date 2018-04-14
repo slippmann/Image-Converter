cc934273447ae10004aea0f2086f9b42463aedd9
slippmann <steven.lippmann1@gmail.com>
2018-04-14 16:32:15 -0700

    Initial Commit
    The basic image converter software

---
+++ Image2Bin/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Image2Bin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Convert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.ImgFile = new System.Windows.Forms.TextBox();
+            this.SaveName = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.EndianCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Convert
+            // 
+            this.Convert.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Convert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert.Location = new System.Drawing.Point(0, 253);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(471, 60);
+            this.Convert.TabIndex = 0;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(298, 54);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(112, 45);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse...";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(13, 13);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(95, 20);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Image2Bin";
+            // 
+            // ImgFile
+            // 
+            this.ImgFile.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ImgFile.Location = new System.Drawing.Point(40, 63);
+            this.ImgFile.Name = "ImgFile";
+            this.ImgFile.ReadOnly = true;
+            this.ImgFile.Size = new System.Drawing.Size(222, 26);
+            this.ImgFile.TabIndex = 3;
+            this.ImgFile.Text = "File to convert...";
+            // 
+            // SaveName
+            // 
+            this.SaveName.Location = new System.Drawing.Point(40, 133);
+            this.SaveName.Name = "SaveName";
+            this.SaveName.ReadOnly = true;
+            this.SaveName.Size = new System.Drawing.Size(222, 26);
+            this.SaveName.TabIndex = 4;
+            this.SaveName.Text = "Save binary as...";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(298, 124);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(112, 45);
+            this.Save.TabIndex = 6;
+            this.Save.Text = "Save As...";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // EndianCheckBox
+            // 
+            this.EndianCheckBox.AutoSize = true;
+            this.EndianCheckBox.Location = new System.Drawing.Point(298, 200);
+            this.EndianCheckBox.Name = "EndianCheckBox";
+            this.EndianCheckBox.Size = new System.Drawing.Size(121, 24);
+            this.EndianCheckBox.TabIndex = 7;
+            this.EndianCheckBox.Text = "Big Endian?";
+            this.EndianCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 313);
+            this.Controls.Add(this.EndianCheckBox);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.SaveName);
+            this.Controls.Add(this.ImgFile);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.Convert);
+            this.Name = "Form1";
+            this.Text = "Image Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.TextBox ImgFile;
+        private System.Windows.Forms.TextBox SaveName;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.CheckBox EndianCheckBox;
+    }
+}
+
---
+++ Image2Bin/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image2Bin
+{
+    public partial class Form1 : Form
+    {
+        private string[] imagePaths = new string[20];
+        private string[] newPaths = new string[20];
+
+        private int height;
+        private int width;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image Files(*.bmp, *.jpg, *.png) | *.bmp; *.jpg; *.png";
+            openFileDialog1.Multiselect = true;
+            var result = openFileDialog1.ShowDialog();
+
+            if(result == DialogResult.OK)
+            {
+                imagePaths = openFileDialog1.FileNames;
+                for (int i = 0; i < imagePaths.Length; i++)
+                {
+                    newPaths[i] = Path.GetDirectoryName(imagePaths[i]) + "\\" + Path.GetFileNameWithoutExtension(imagePaths[i]) + ".bin";
+                }
+            }
+
+            Save.Enabled = (imagePaths.Length <= 1);
+            SaveName.Enabled = (imagePaths.Length > 1);
+
+            ImgFile.Text = Path.GetFileName(imagePaths[0]);
+            SaveName.Text = Path.GetFileName(newPaths[0]);
+
+            ImgFile.Text += (imagePaths.Length > 1) ? ", ..." : "";
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < imagePaths.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(newPaths[i]))
+                {
+                    if (!string.IsNullOrWhiteSpace(SaveName.Text) &&
+                        SaveName.Text != "Save binary as...")
+                    {
+                        newPaths[i] = Path.GetDirectoryName(imagePaths[i]) + "\\" + SaveName.Text;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(imagePaths[i]) &&
+                    File.Exists(imagePaths[i]))
+                {
+                    var image = new Bitmap(imagePaths[i]);
+                    var length = image.Height * image.Width * 2;
+                    var rgb = new byte[length + 4];
+                    var index = 0;
+
+#if DEBUG
+                    var rgb_st = new string[image.Height];
+#endif
+
+                    height = image.Height;
+                    width = image.Width;
+
+                    rgb[index++] = (byte)(height >> 8 & 0xFF);
+                    rgb[index++] = (byte)(height & 0xFF);
+                    rgb[index++] = (byte)(width >> 8 & 0xFF);
+                    rgb[index++] = (byte)(width & 0xFF);
+
+#if DEBUG
+                    rgb_st[0] += String.Format("0x{0:X2}", rgb[0]) + String.Format("{0:X2},", rgb[1]);
+                    rgb_st[0] += String.Format("0x{0:X2}", rgb[2]) + String.Format("{0:X2},\n", rgb[3]);
+#endif
+
+
+                    for (int y = 0; y < image.Height; y++)
+                    {
+                        for (int x = 0; x < image.Width; x++)
+                        {
+                            var colour = image.GetPixel(x, y);
+
+                            var ar = (colour.A & 0xF0) | ((colour.R & 0xF0) >> 4);
+                            var gb = (colour.G & 0xF0) | ((colour.B & 0xF0) >> 4);
+
+                            rgb[index++] = (byte)ar;
+                            rgb[index++] = (byte)gb;
+
+#if DEBUG
+                            rgb_st[y] += String.Format("0x{0:X2}", ar) + String.Format("{0:X2}", gb);
+
+                            if (x != (image.Width - 1) || y != (image.Height - 1))
+                            {
+                                rgb_st[y] += ", ";
+                            }
+#endif
+                        }
+
+#if DEBUG
+                        if (y < (image.Height - 1))
+                        {
+                            rgb_st[y] += "\\";
+                        }
+#endif
+                    }
+
+                    var newRgb = rgb;
+
+                    if (EndianCheckBox.Checked == false)
+                    {
+                        newRgb = ToLittleEndian(rgb);
+                    }
+
+                    File.WriteAllBytes(newPaths[i], newRgb);
+
+#if DEBUG
+                    var textFile = Path.GetDirectoryName(newPaths[i]) + "\\" + Path.GetFileNameWithoutExtension(newPaths[i]) + ".txt";
+                    File.WriteAllLines(textFile, rgb_st);
+#endif
+                }
+
+            }
+
+            System.Diagnostics.Process.Start("explorer.exe", Path.GetDirectoryName(newPaths[0]));
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(SaveName.Text) &&
+                    SaveName.Text != "Save binary as...")
+            {
+                saveFileDialog1.FileName = SaveName.Text;
+            }
+
+            saveFileDialog1.DefaultExt = "bin";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.Filter = "Binary File(*.bin) | *.bin | All Files(*.*) | *.*";
+
+            var result = saveFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                newPaths[0] = saveFileDialog1.FileName;
+            }
+
+            SaveName.Text = Path.GetFileName(newPaths[0]);
+        }
+
+        private byte[] ToLittleEndian(byte[] array)
+        {
+            int extra = array.Length % 4;
+            var newArray = new byte[array.Length + extra];
+
+            for (int i = 0; i < newArray.Length; i += 4)
+            {
+                var temp1 = array[i];
+                var temp2 = array[i + 1];
+
+                if (i + 2 >= array.Length)
+                {
+                    newArray[i] = 0;
+                    newArray[i + 1] = 0;
+                }
+                else
+                {
+                    newArray[i] = array[i + 3];
+                    newArray[i + 1] = array[i + 2];
+                }
+
+                newArray[i + 2] = temp2;
+                newArray[i + 3] = temp1;
+            }
+
+            return newArray;
+        }
+    }
+}
